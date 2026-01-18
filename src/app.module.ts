c7ec5cc687e7a99f03b9032fc1ddce203bec70dd
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2026-01-18 13:24:53 +0200

    feat: Configure global API prefix, enable CORS, and expand format script scope.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,22 +7,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({
-    isGlobal: true,
-  }),
-TypeOrmModule.forRootAsync({
-  imports: [ConfigModule],
-  inject: [ConfigService],
-  useFactory: (config: ConfigService) => {
-    return {
-    type: "postgres",
-    url: config.getOrThrow("DATABASE_URL"),
-    autoLoadEntities: true,
-    synchronize: false,
-  }
-  },
-}),
-
-],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          url: config.getOrThrow('DATABASE_URL'),
+          autoLoadEntities: true,
+          synchronize: false,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
