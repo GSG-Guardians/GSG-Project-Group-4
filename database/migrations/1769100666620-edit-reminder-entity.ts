c73eb9fa18145515a555df8449dc130ca4461051
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2026-01-22 18:56:49 +0200

    formatting..

--- database/migrations/1769100666620-edit-reminder-entity.ts
+++ database/migrations/1769100666620-edit-reminder-entity.ts
@@ -1,18 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class EditReminderEntity1769100666620 implements MigrationInterface {
-    name = 'EditReminderEntity1769100666620'
+  name = 'EditReminderEntity1769100666620';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "reminders" DROP COLUMN "title"`);
-        await queryRunner.query(`ALTER TABLE "reminders" DROP COLUMN "amount"`);
-        await queryRunner.query(`ALTER TABLE "reminders" DROP COLUMN "currency"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "reminders" DROP COLUMN "title"`);
+    await queryRunner.query(`ALTER TABLE "reminders" DROP COLUMN "amount"`);
+    await queryRunner.query(`ALTER TABLE "reminders" DROP COLUMN "currency"`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "reminders" ADD "currency" character(3)`);
-        await queryRunner.query(`ALTER TABLE "reminders" ADD "amount" numeric(14,2)`);
-        await queryRunner.query(`ALTER TABLE "reminders" ADD "title" character varying(160) NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "reminders" ADD "currency" character(3)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reminders" ADD "amount" numeric(14,2)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reminders" ADD "title" character varying(160) NOT NULL`,
+    );
+  }
 }
