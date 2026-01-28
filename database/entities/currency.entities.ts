9639d2837543dd38f7c5baf9c3deaca322c6bee5
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2026-01-28 11:05:44 +0200

    formatting

--- database/entities/currency.entities.ts
+++ database/entities/currency.entities.ts
@@ -3,15 +3,15 @@ import { Column, Entity, PrimaryGeneratedColumn, Index } from 'typeorm';
 @Entity({ name: 'currencies' })
 @Index(['code'], { unique: true })
 export class Currency {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column('varchar', { length: 3, unique: true })
-    code: string;
+  @Column('varchar', { length: 3, unique: true })
+  code: string;
 
-    @Column('varchar', { length: 10, nullable: true })
-    symbol: string | null;
+  @Column('varchar', { length: 10, nullable: true })
+  symbol: string | null;
 
-    @Column('varchar', { length: 100 })
-    name: string;
+  @Column('varchar', { length: 100 })
+  name: string;
 }
